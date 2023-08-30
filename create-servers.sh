be7ac1e9f5a9a0be53495db138a5a1916834a027
sivakumarreddy <info@joindevops.com>
2023-08-30 07:33:32 +0530

    ansible

--- create-servers.sh
+++ create-servers.sh
@@ -5,6 +5,7 @@ INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-0189d4870cc65fd55
 DOMAIN_NAME=joindevops.online
+HOSTED_ZONE_ID=Z0308214GYCUYHGJHT8R
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
@@ -20,7 +21,7 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
 
-    aws route53 change-resource-record-sets --hosted-zone-id Z098285010FMU6PDV8O9P --change-batch '
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
     {
             "Changes": [{
             "Action": "CREATE",
@@ -36,4 +37,4 @@ done
 
 # imporvement
 # check instance is already created or not
-# update route53 record
+# check route53 record is already exist, if exist update, otherwise create route53 record
