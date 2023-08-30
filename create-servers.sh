ca953d6f3324f0f419fc5fd2b75ab84314c43f6d
sivakumarreddy <info@joindevops.com>
2023-08-30 07:30:28 +0530

    ansible

--- create-servers.sh
+++ create-servers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+NAMES=$@
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-0189d4870cc65fd55
@@ -8,7 +8,7 @@ DOMAIN_NAME=joindevops.online
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
-for i in "${NAMES[@]}"
+for i in $@
 do  
     if [[ $i == "mongodb" || $i == "mysql" ]]
     then
